25cb810dc06bc7808b40320e7c69f06ef518235f
Ahmed Adib <ahmedadib0179@gmail.com>
2021-08-04 09:51:40 +0600

    React Calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import ReactDOM from "react-dom";
+function Adib() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+  const ops = ["/", "*", "+", "-", "."];
+  const updateCalc = (value) => {
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  };
+  const createDigits = () => {
+    const digits = [];
+    for (let i = 0; i < 10; i++) {
+      digits.push(
+        <button onClick={() => updateCalc(i.toString())} key={i}>
+          {i}
+        </button>
+      );
+    }
+    return digits;
+  };
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='calculator'>
+        <div className='display'>
+          {result ? <span>{result}</span> : ""}
+          {calc || "0"}
+        </div>
+        <div className='operators'>
+          <button onClick={() => updateCalc("/")}>/</button>
+          <button onClick={() => updateCalc("*")}>*</button>
+          <button onClick={() => updateCalc("-")}>-</button>
+
+          <button onClick={() => updateCalc("+")}>+</button>
+          <button onClick={() => updateCalc("+")}></button>
+        </div>
+        <div className='digits'>
+          {createDigits()}
+
+          <button onClick={() => updateCalc(".")}>.</button>
+          <button></button>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Adib;
